4201a8d1f3c879bdc994dba581da66854a39ca8c
winniekiarie <kiariebsc@gmail.com>
2023-08-16 12:23:01 -0700

    top score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script to List Records of second_table
+-- This script lists all records of the second_table from the hbtn_0c_0 database.
+-- Results display both the score and the name, ordered by score (top first).
+
+SELECT score, name FROM second_table ORDER BY score DESC;
