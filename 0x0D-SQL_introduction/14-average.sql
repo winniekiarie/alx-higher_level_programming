68dfc0839d723988dd80b736b45f33a7279cbdc0
winniekiarie <kiariebsc@gmail.com>
2023-08-16 12:34:50 -0700

    average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Script to Compute Average Score of all Records in second_table
+-- from the hbtn_0c_0 database and displays the result column named "average".
+
+
+SELECT AVG(score) AS average FROM second_table;
