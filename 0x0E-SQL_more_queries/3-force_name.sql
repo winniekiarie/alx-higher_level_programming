578bff6bb352d37b819a8731d0bbf615a4a8f988
winniekiarie <kiariebsc@gmail.com>
2023-08-16 11:30:26 -0700

    force name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Create Table force_name
+-- Execute: cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
