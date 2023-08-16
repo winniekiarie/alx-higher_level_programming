5d95533c9a5f5fc568045ba13dd1f8b4b2611cf0
winniekiarie <kiariebsc@gmail.com>
2023-08-16 11:45:08 -0700

    genre id

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id
+-- Execute: cat 13-shows_with_genre.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
