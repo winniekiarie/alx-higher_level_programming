5d3a701c1c3fc24783f74b7f41d29f78c0ae2e4e
winniekiarie <kiariebsc@gmail.com>
2023-10-02 12:06:21 -0700

    Network 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""A script that:
-- takes your GitHub credentials (username and password)
-- uses the GitHub API to display your id
 """
+A Python script that takes GitHub credentials
+(username and personal access token)
+and uses Basic Authentication to access the
+GitHub API and display your GitHub User ID.
+
+Requirements:
+- Use the requests package.
+- Do not import any packages other than requests and sys.
+
+Usage:
+./github_user_id.py <username> <personal_access_token>
+"""
+
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
-
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
-"""A script tha:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
 """
+A Python script that sends a POST request to http://0.0.0.0:5000/search_user
+with a letter as a parameter and processes the response.
+
+Requirements:
+- Use the requests package.
+- Do not import any packages other than requests and sys.
+
+Usage:
+./search_user.py <letter>
+"""
+
 import sys
 import requests
 
-
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    # Check if a letter argument was provided, default to empty string if not
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    url = "http://0.0.0.0:5000/search_user"
+    data = {'q': letter}
+
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        response = requests.post(url, data=data)
+        response_json = response.json()
+
+        if response_json:
+            print("[{}] {}".format(response_json['id'], response_json['name']))
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("No result")
+
     except ValueError:
         print("Not a valid JSON")
