675b762ea4c84fcd70f12dd6e410baa98173fdcb
winniekiarie <kiariebsc@gmail.com>
2023-10-02 11:54:42 -0700

    Network 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
 """
-import sys
-import urllib.parse
-import urllib.request
+A Python script that sends a POST request to a URL with an email parameter
+and displays the body of the response (decoded in utf-8).
+"""
 
+import urllib.request
+import urllib.parse
+import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    # Check if both URL and email arguments were provided
+    if len(sys.argv) != 3:
+        sys.exit("Usage: ./2-post_email.py <URL> <email>")
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Prepare the data to be sent in the POST request
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            # Read and decode the response body in utf-8
+            response_content = response.read().decode('utf-8')
+            print(response_content)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
